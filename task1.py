c7107a1c776a39430fa4b5c6cf0cc2f41b5b5cfb
PriyanshuSahu234 <priyanshbhoyare@gmail.com>
2026-02-16 12:59:12 +0530

    task1

---
+++ task1.py
@@ -0,0 +1,12 @@
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+sum=a+b
+print("\nThe sum of",a,"and",b,"is :",sum)
+diff=a-b
+print("The difference of",a,"and",b,"is:",diff)
+product=a*b
+print("The product of",a,"and",b,"is:",product)
+if b!=0:
+    quotient=a/b
+    print("The quotient of",a,"and",b,"is:",quotient)
+else:    print("Cannot divide by zero")
