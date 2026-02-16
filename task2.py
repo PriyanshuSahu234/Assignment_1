79e95b701ddbd3c871867e3cb5264715d8e532a4
PriyanshuSahu234 <priyanshbhoyare@gmail.com>
2026-02-16 13:30:17 +0530

    task2

---
+++ task2.py
@@ -0,0 +1,4 @@
+fristName=input("Enter your first name: ")
+lastName=input("Enter your last name: ")
+fullName=fristName+" "+lastName
+print("Hello, ",fullName,"! Welcome to the python programming!")
